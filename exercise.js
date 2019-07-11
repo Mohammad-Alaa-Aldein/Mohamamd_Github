fa7404b228f32fd3a9291554e0b80050a65e6ba0
Mohammad-Alaa-Aldein <51743463+Mohammad-Alaa-Aldein@users.noreply.github.com>
2019-07-11 10:36:22 +0300

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,16 @@
+function tellFortune ( jobTitle,  geographicLocation,  parentsName,numberOfChildren ){
+
+
+
+
+
+console.log(tellFortune ("You will be a" + jobTitle + "in" + geographicLocation + ", and married to " + parentsName + numberOfChildren );
+}
+
+	tellFortune( "Software Engineer" , "Jordan" , "Alis" , 3 );
+
+
+
+
+
+	
